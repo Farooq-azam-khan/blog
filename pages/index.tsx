4c4020fb0d6da5d59da4a83242d57bfb2ceab60d
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-11-11 10:40:49 -0500

    refactor + new blog started

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,102 +1,115 @@
-import type { NextPage } from 'next'
-import Navbar from '../components/Navbar'
+import type { NextPage } from "next";
+import Navbar from "../components/Navbar";
+import { meta as fli_meta } from "./fast-llm-inferencing.mdx";
+import { meta as cos_meta } from "./cosine-similarity.mdx";
+import { meta as cos_pt2_meta } from "./cosine-similarity-pt-2.mdx";
+import { meta as tfidf_meta } from "./tfidf.mdx";
+import { meta as tech_stack_meta } from "./tech-stack.mdx";
 
-// import {meta as tech_stack} from '/home/farooq-azam-khan/Documents/dev/blog/pages/cosine-similarity.mdx'
 type BlogDate = {
-  month: string, year: number, date: string
-}
+  month: string;
+  year: number;
+  date: string;
+};
 type BlogMetaData = {
-  title: string,
-  summary: string,
-  post_link: string,
-  published_date: BlogDate
-}
+  title: string;
+  summary: string;
+  post_link: string;
+  published_date: BlogDate;
+};
 
+// export const cos_meta = {
+//   title: "Comparing Vectors with Cosine Simlarity Function",
+//   published_date: { month: "July", date: "4th", year: 2022 },
+//   summary:
+//     "This tutorial will focus on the math behind text vector similarity using numpy, pytorch, and stentence-transformers libraries in python.",
+//   post_link: "cosine-similarity",
+// };
 
-export const cos_meta = {
-  title: "Comparing Vectors with Cosine Simlarity Function",
-  published_date: { month: "July", date: "4th", year: 2022 }
-  , summary: "This tutorial will focus on the math behind text vector similarity using numpy, pytorch, and stentence-transformers libraries in python."
-  , post_link: "cosine-similarity"
-}
+// export const cos_pt2_meta = {
+//   title: "Large Scale Vector Comparison",
+//   published_date: { month: "July", date: "9th", year: 2022 },
+//   summary:
+//     "In this post, we will look at the quora qna dataset and aim to encode and compare all question pairs. The purpose of is to look at a real dataset.",
+//   post_link: "cosine-similarity-pt-2",
+// };
 
-export const cos_pt2_meta = {
-  title: "Large Scale Vector Comparison",
-  published_date: { month: "July", date: "9th", year: 2022 }
-  , summary: "In this post, we will look at the quora qna dataset and aim to encode and compare all question pairs. The purpose of is to look at a real dataset."
-  , post_link: "cosine-similarity-pt-2"
-}
+// export const tfidf_meta = {
+//   title: "Term Frequency-Inverse Document Frequency",
+//   published_date: { month: "August", date: "1st", year: 2022 },
+//   summary:
+//     "In this tutorial we will look at what TF and IDF are and how they can be use to process text data in Machine learning.",
+//   post_link: "tfidf",
+// };
 
-export const tfidf_meta = {
-  title: "Term Frequency-Inverse Document Frequency",
-  published_date: { month: "August", date: "1st", year: 2022 }
-  , summary: "In this tutorial we will look at what TF and IDF are and how they can be use to process text data in Machine learning."
-  , post_link: "tfidf"
-}
-
-export const tech_stack_meta = {
-  title : "Tech stack for rapid prototyping applications", 
-  published_date : { month : "June", date : "3rd", year : 2023 }
-  , summary : "In this blog I will focus on the ways in which I set up a backend and frontend to do rapid prototyping of full stack applications."
-  , post_link : "tech-stack"
-}
+// export const tech_stack_meta = {
+//   title: "Tech stack for rapid prototyping applications",
+//   published_date: { month: "June", date: "3rd", year: 2023 },
+//   summary:
+//     "In this blog I will focus on the ways in which I set up a backend and frontend to do rapid prototyping of full stack applications.",
+//   post_link: "tech-stack",
+// };
 
 const BlogPostView = ({ meta_data }: { meta_data: BlogMetaData }) => {
-  const display_str = display_publication_date(meta_data.published_date)
-  return (<div className='hover:bg-orange-100 py-2 rounded hover:rounded-lg ease-in duration-200 border-l-4  border-white hover:border-indigo-400 px-3 flex flex-col space-y-2'>
-    <span className='text-indigo-600'> {display_str}
-    </span>
-    <span className='mt-3'>
-      <a href={meta_data.post_link}>
-        {meta_data.title}
-      </a>
-    </span>
-    <span className="text-gray-700">{meta_data.summary}</span>
-  </div>)
-}
-
+  const display_str = display_publication_date(meta_data.published_date);
+  return (
+    <div className="hover:bg-orange-100 py-2 rounded hover:rounded-lg ease-in duration-200 border-l-4  border-white hover:border-indigo-400 px-3 flex flex-col space-y-2">
+      <span className="text-indigo-600"> {display_str}</span>
+      <span className="mt-3">
+        <a href={meta_data.post_link}>{meta_data.title}</a>
+      </span>
+      <span className="text-gray-700">{meta_data.summary}</span>
+    </div>
+  );
+};
 
 function get_date_obj(published_date: BlogDate): Date {
   // 'st', 'th', 'nd', 'rd'
-  let date = published_date.date
-  date = date.replace('st', '')
-  date = date.replace('nd', '')
-  date = date.replace('rd', '')
-  date = date.replace('th', '')
-  const b1_date_obj = new Date(published_date.year, getMonth(published_date.month), +date)
+  let date = published_date.date;
+  date = date.replace("st", "");
+  date = date.replace("nd", "");
+  date = date.replace("rd", "");
+  date = date.replace("th", "");
+  const b1_date_obj = new Date(
+    published_date.year,
+    getMonth(published_date.month),
+    +date
+  );
 
-  return b1_date_obj
-
+  return b1_date_obj;
 }
 function BlogPostListView({ blogs }: { blogs: BlogMetaData[] }) {
-  let blogs_clone = [...blogs]
+  let blogs_clone = [...blogs];
   blogs_clone.sort((blog1: BlogMetaData, blog2: BlogMetaData) => {
-    const b1_publised_date = get_date_obj(blog1.published_date)
-    const b2_publised_date = get_date_obj(blog2.published_date)
-    return b2_publised_date.getTime() - b1_publised_date.getTime()
-  })
-  return (<>
-    {blogs_clone.map((blog) => {
-      return <BlogPostView meta_data={blog} key={blog.title} />
-    })}
-  </>)
-
+    const b1_publised_date = get_date_obj(blog1.published_date);
+    const b2_publised_date = get_date_obj(blog2.published_date);
+    return b2_publised_date.getTime() - b1_publised_date.getTime();
+  });
+  return (
+    <>
+      {blogs_clone.map((blog) => {
+        return <BlogPostView meta_data={blog} key={blog.title} />;
+      })}
+    </>
+  );
 }
-
 
 const Home: NextPage = () => {
   return (
     <div className="sm:mx-0 md:mx-auto prose lg:prose-lg sm:max-w-xl lg:max-w-3xl mt-10">
       <Navbar />
       <section className="space-y-2">
-        <BlogPostListView blogs={[cos_pt2_meta, 
-          cos_meta, 
-          tfidf_meta, 
-          tech_stack_meta
-          ]} />
-
+        <BlogPostListView
+          blogs={[
+            fli_meta,
+            cos_pt2_meta,
+            cos_meta,
+            tfidf_meta,
+            tech_stack_meta,
+          ]}
+        />
       </section>
-      <section>
+      {/* <section>
         <h2 className='text-gray-700'>Drafts</h2>
         <ul className='list-disc'>
           <li>Singular Value Decomposition and Recommendation Engines</li>
@@ -107,42 +120,42 @@ const Home: NextPage = () => {
           <li>K-arm bandit</li>
           <li>anomaly detection with gaussian distribution and multi-variate gaussian distribution</li>
         </ul>
-      </section>
+      </section> */}
     </div>
-  )
-}
+  );
+};
 
 function display_publication_date(pub_date: BlogDate) {
   return pub_date.month + " " + pub_date.date + ", " + pub_date.year;
 }
 
 function getMonth(month: string): number {
-  if (month == 'January' || month == 'Jan') {
-    return 0
-  } else if (month == 'February' || month == 'Feb') {
-    return 1
-  } else if (month == 'March' || month == 'Mar') {
-    return 2
-  } else if (month == 'April' || month == 'Apr') {
-    return 3
-  } else if (month == 'May') {
-    return 4
-  } else if (month == 'June') {
-    return 5
-  } else if (month == 'July') {
-    return 6
-  } else if (month == 'August' || month == 'Aug') {
-    return 7
-  } else if (month == 'September' || month == 'Sept') {
-    return 8
-  } else if (month == 'October' || month == 'Oct') {
-    return 9
-  } else if (month == 'November' || month == 'Nov') {
-    return 10
-  } else if (month == 'December' || month == 'Dec') {
-    return 11
+  if (month == "January" || month == "Jan") {
+    return 0;
+  } else if (month == "February" || month == "Feb") {
+    return 1;
+  } else if (month == "March" || month == "Mar") {
+    return 2;
+  } else if (month == "April" || month == "Apr") {
+    return 3;
+  } else if (month == "May") {
+    return 4;
+  } else if (month == "June") {
+    return 5;
+  } else if (month == "July") {
+    return 6;
+  } else if (month == "August" || month == "Aug") {
+    return 7;
+  } else if (month == "September" || month == "Sept") {
+    return 8;
+  } else if (month == "October" || month == "Oct") {
+    return 9;
+  } else if (month == "November" || month == "Nov") {
+    return 10;
+  } else if (month == "December" || month == "Dec") {
+    return 11;
   }
-  return 0
+  return 0;
 }
 
-export default Home
+export default Home;

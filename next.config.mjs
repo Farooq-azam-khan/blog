dd9e1fa39f3e2587c7ab9999173d3625946d225d
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-11-19 14:08:07 -0500

    mjs + cjs

---
+++ next.config.mjs
@@ -0,0 +1,34 @@
+import createMDX from '@next/mdx'
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    // Append the default value with md extensions
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        formats: ["image/avif", "image/webp"],
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'avatars.githubusercontent.com',
+            port: '',
+            pathname: '/u/**',
+        }]
+    }
+}
+
+// next.config.js
+
+const withMDX = createMDX({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+        // mdxRs: true, // experimental rust based compiler
+        // If you use `MDXProvider`, uncomment the following line.
+        providerImportSource: "@mdx-js/react",
+    }
+})
+export default withMDX(nextConfig)
+
+
